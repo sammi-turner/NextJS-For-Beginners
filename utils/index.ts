335c10c04410d269f7ae90bde6086706f781c08d
Sammi Turner <ms.sammi.turner@gmail.com>
2025-06-10 21:48:08 +0100

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,7 @@
-export const sortByDate = (a: any, b: any): number => {
+export const sortByDate = (
+  a: { frontmatter: { date: string | number | Date } },
+  b: { frontmatter: { date: string | number | Date } }
+): number => {
   return (
     (new Date(a.frontmatter.date) as unknown as number) -
     (new Date(b.frontmatter.date) as unknown as number)
