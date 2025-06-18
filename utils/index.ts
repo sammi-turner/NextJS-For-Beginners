8d94aaa6201beef72b32164700b2faa47efc426e
Sammi Turner <ms.sammi.turner@gmail.com>
2025-06-18 15:16:54 +0100

    Delete utils directory

--- utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const sortByDate = (
-  a: { frontmatter: { date: string | number | Date } },
-  b: { frontmatter: { date: string | number | Date } }
-): number => {
-  return (
-    (new Date(a.frontmatter.date) as unknown as number) -
-    (new Date(b.frontmatter.date) as unknown as number)
-  );
-};
