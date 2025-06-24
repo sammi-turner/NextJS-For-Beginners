52c5fa18bf388b7a560c0f2390bf9f69817a81b3
Sammi Turner <ms.sammi.turner@gmail.com>
2025-06-25 00:40:36 +0100

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -9,6 +10,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <Header />
       <main className="container">
         <Component {...pageProps} />
+        <Analytics />
       </main>
       <Footer />
     </div>
